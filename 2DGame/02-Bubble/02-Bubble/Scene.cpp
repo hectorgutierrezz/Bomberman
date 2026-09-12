5e0463117ef06f3310ffeac3722f7a0b6483b2c5
Jan <jan.diez.de.los.rios@estudiantat.upc.edu>
2026-09-12 14:28:00 +0200

    Assets varios afegits

--- 2DGame/02-Bubble/02-Bubble/Scene.cpp
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -11,13 +11,21 @@
 #define INIT_PLAYER_X_TILES 4
 #define INIT_PLAYER_Y_TILES 25
 
+enum GameState {
+	MAIN_MENU,
+	INSTRUCTIONS,
+	CREDITS,
+	PLAYING,
+	PAUSED,
+	GAME_OVER,
+	WIN
+};
+
+
 Scene::Scene()
 {
 	map = NULL;
 	player = NULL;
-	gameState = MAIN_MENU;
-	for (int i = 0; i <= GLFW_KEY_LAST; ++i)
-		keyLastState[i] = false;
 }
 
 Scene::~Scene()
@@ -33,9 +41,6 @@ Scene::~Scene()
 void Scene::init()
 {
 	initShaders();
-	gameState = MAIN_MENU;
-	for (int i = 0; i <= GLFW_KEY_LAST; ++i)
-		keyLastState[i] = false;
 	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
 	player = new Player();
 	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
@@ -45,74 +50,53 @@ void Scene::init()
 	currentTime = 0.0f;
 }
 
-bool Scene::isKeyJustPressed(int key)
-{
-	if (key < 0 || key > GLFW_KEY_LAST)
-		return false;
-	bool isDown = Game::instance().getKey(key);
-	bool justPressed = isDown && !keyLastState[key];
-	keyLastState[key] = isDown;
-	return justPressed;
-}
-
 void Scene::update(int deltaTime)
 {
 	switch (gameState) {
 		case MAIN_MENU:
-			if (isKeyJustPressed(GLFW_KEY_ENTER) || isKeyJustPressed(GLFW_KEY_SPACE)) {
+			if(Game::instance().keyPressed()) {
 				gameState = PLAYING;
 			}
-			else if (isKeyJustPressed(GLFW_KEY_I)) {
-				gameState = INSTRUCTIONS;
-			}
-			else if (isKeyJustPressed(GLFW_KEY_C)) {
-				gameState = CREDITS;
-			}
-			break;
+		break;
 
 		case INSTRUCTIONS:
-			if (isKeyJustPressed(GLFW_KEY_ENTER) || isKeyJustPressed(GLFW_KEY_ESCAPE)) {
+			if(Game::instance().getKey(GLFW_KEY_ENTER)) {
 				gameState = MAIN_MENU;
 			}
-			break;
+		break;
 
 		case CREDITS:
-			if (isKeyJustPressed(GLFW_KEY_ENTER) || isKeyJustPressed(GLFW_KEY_ESCAPE)) {
+			if(Game::instance().getKey(GLFW_KEY_ENTER)) {
 				gameState = MAIN_MENU;
 			}
-			break;
+		break;
 
 		case PLAYING:
-			if (isKeyJustPressed(GLFW_KEY_P)) {
+			if(Game::instance().keyPressed('esc')) {
 				gameState = PAUSED;
 			}
-			else {
-				currentTime += deltaTime;
-				player->update(deltaTime);
-			}
-			break;
+		break;
 
 		case PAUSED:
-			if (isKeyJustPressed(GLFW_KEY_P) || isKeyJustPressed(GLFW_KEY_ENTER)) {
+			if(Game::instance().getKey(GLFW_KEY_ENTER)) {
 				gameState = PLAYING;
 			}
-			else if (isKeyJustPressed(GLFW_KEY_M) || isKeyJustPressed(GLFW_KEY_ESCAPE)) {
-				gameState = MAIN_MENU;
-			}
-			break;
+		break;
 
 		case GAME_OVER:
-			if (isKeyJustPressed(GLFW_KEY_ENTER) || isKeyJustPressed(GLFW_KEY_ESCAPE)) {
+			if(Game::instance().getKey(GLFW_KEY_ENTER)) {
 				gameState = MAIN_MENU;
 			}
-			break;
+		break;
 
 		case WIN:
-			if (isKeyJustPressed(GLFW_KEY_ENTER) || isKeyJustPressed(GLFW_KEY_ESCAPE)) {
+			if(Game::instance().getKey(GLFW_KEY_ENTER)) {
 				gameState = MAIN_MENU;
 			}
-			break;
+		break;
 	}
+	currentTime += deltaTime;
+	player->update(deltaTime);
 }
 
 void Scene::render()
@@ -125,11 +109,8 @@ void Scene::render()
 	modelview = glm::mat4(1.0f);
 	texProgram.setUniformMatrix4f("modelview", modelview);
 	texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
-
-	if (gameState == PLAYING || gameState == PAUSED) {
-		map->render();
-		player->render();
-	}
+	map->render();
+	player->render();
 }
 
 void Scene::initShaders()
